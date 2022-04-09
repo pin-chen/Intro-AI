b88b85658e3bcc3d1b482b4fd4e14542b33f9b40
Pin-Shao Chen <hardymike6308@gmail.com>
2022-04-10 04:42:41 +0800

    HW3

--- HW3/AI_HW3/multiAgents.py
+++ HW3/AI_HW3/multiAgents.py
@@ -59,7 +59,7 @@ class ReflexAgent(Agent):
         newFoodsDistances = [manhattanDistance(newPos, food) for food in newFood.asList()]
         newNearestFoodDistance = 0 if not newFoodsDistances else min(newFoodsDistances)
         isFoodNearer = nearestFoodDistance - newNearestFoodDistance
-
+            
         direction = currentGameState.getPacmanState().getDirection()
         if minGhostDistance <= 1 or action == Directions.STOP:
             return 0
@@ -134,10 +134,44 @@ class MinimaxAgent(MultiAgentSearchAgent):
         Returns whether or not the game state is a losing state
         """
         # Begin your code (Part 1)
-        raise NotImplementedError("To be implemented")
+        #raise NotImplementedError("To be implemented")
+        _, nextAction = self.maxValue(gameState, 0, 0);
+        return nextAction
+    def terminalState(self, gameState, agentIndex, curDepth):
+        if gameState.isWin() or gameState.isLose() or curDepth >= self.depth:
+            return True
+        return False
+    def nextValue(self, gameState, agentIndex, curDepth, nextAgent, legalAction):
+        nextState = gameState.getNextState(agentIndex, legalAction)
+        legalValue = float()
+        if nextAgent == 0:
+            legalValue, _ = self.maxValue(nextState, nextAgent, curDepth + 1)
+        else:
+            legalValue = self.minValue(nextState, nextAgent, curDepth)
+        return legalValue
+    def maxValue(self, gameState, agentIndex, curDepth):
+        if( self.terminalState(gameState, agentIndex, curDepth) ):
+            return self.evaluationFunction(gameState), None
+        nextAgent = (agentIndex + 1) % gameState.getNumAgents()
+        scores = list()
+        legalMoves = gameState.getLegalActions(agentIndex)
+        for legalAction in legalMoves:
+            legalValue = self.nextValue(gameState, agentIndex, curDepth, nextAgent, legalAction)
+            scores.append(legalValue)
+        bestScore = max(scores)
+        bestIndices = [index for index in range(len(scores)) if scores[index] == bestScore]
+        chosenIndex = random.choice(bestIndices)
+        return bestScore, legalMoves[chosenIndex]
+    def minValue(self, gameState, agentIndex, curDepth):
+        if( self.terminalState(gameState, agentIndex, curDepth) ):
+            return self.evaluationFunction(gameState)
+        nextAgent = (agentIndex + 1) % gameState.getNumAgents()
+        bestScore = float("inf")
+        for legalAction in gameState.getLegalActions(agentIndex):
+            legalValue = self.nextValue(gameState, agentIndex, curDepth, nextAgent, legalAction)
+            bestScore = min(bestScore, legalValue)
+        return bestScore
         # End your code (Part 1)
-
-
 class AlphaBetaAgent(MultiAgentSearchAgent):
     """
     Your minimax agent with alpha-beta pruning (Part 2)
@@ -148,7 +182,49 @@ class AlphaBetaAgent(MultiAgentSearchAgent):
         Returns the minimax action using self.depth and self.evaluationFunction
         """
         # Begin your code (Part 2)
-        raise NotImplementedError("To be implemented")
+        #raise NotImplementedError("To be implemented")
+        _, nextAction = self.maxValue(gameState, 0, 0, float("-inf"), float("inf"));
+        return nextAction
+    def terminalState(self, gameState, agentIndex, curDepth):
+        if gameState.isWin() or gameState.isLose() or curDepth >= self.depth:
+            return True
+        return False
+    def nextValue(self, gameState, agentIndex, curDepth, nextAgent, legalAction, alpha, beta):
+        nextState = gameState.getNextState(agentIndex, legalAction)
+        legalValue = float()
+        if nextAgent == 0:
+            legalValue, _ = self.maxValue(nextState, nextAgent, curDepth + 1, alpha, beta)
+        else:
+            legalValue = self.minValue(nextState, nextAgent, curDepth, alpha, beta)
+        return legalValue
+    def maxValue(self, gameState, agentIndex, curDepth, alpha, beta):
+        if( self.terminalState(gameState, agentIndex, curDepth) ):
+            return self.evaluationFunction(gameState), None
+        nextAgent = (agentIndex + 1) % gameState.getNumAgents()
+        scores = list()
+        legalMoves = gameState.getLegalActions(agentIndex)
+        for legalAction in legalMoves:
+            legalValue = self.nextValue(gameState, agentIndex, curDepth, nextAgent, legalAction, alpha, beta)
+            scores.append(legalValue)
+            if legalValue > beta:
+                return legalValue, legalAction
+            alpha = max(alpha, legalValue)
+        bestScore = max(scores)
+        bestIndices = [index for index in range(len(scores)) if scores[index] == bestScore]
+        chosenIndex = random.choice(bestIndices)
+        return bestScore, legalMoves[chosenIndex]
+    def minValue(self, gameState, agentIndex, curDepth, alpha, beta):
+        if( self.terminalState(gameState, agentIndex, curDepth) ):
+            return self.evaluationFunction(gameState)
+        nextAgent = (agentIndex + 1) % gameState.getNumAgents()
+        bestScore = float("inf")
+        for legalAction in gameState.getLegalActions(agentIndex):
+            legalValue = self.nextValue(gameState, agentIndex, curDepth, nextAgent, legalAction, alpha, beta)
+            bestScore = min(bestScore, legalValue)
+            if legalValue < alpha:
+                return legalValue
+            beta = min(beta, bestScore)
+        return bestScore
         # End your code (Part 2)
 
 
@@ -165,7 +241,44 @@ class ExpectimaxAgent(MultiAgentSearchAgent):
         legal moves.
         """
         # Begin your code (Part 3)
-        raise NotImplementedError("To be implemented")
+        #raise NotImplementedError("To be implemented")
+        _, nextAction = self.maxValue(gameState, 0, 0);
+        return nextAction
+    def terminalState(self, gameState, agentIndex, curDepth):
+        if gameState.isWin() or gameState.isLose() or curDepth >= self.depth:
+            return True
+        return False
+    def nextValue(self, gameState, agentIndex, curDepth, nextAgent, legalAction):
+        nextState = gameState.getNextState(agentIndex, legalAction)
+        legalValue = float()
+        if nextAgent == 0:
+            legalValue, _ = self.maxValue(nextState, nextAgent, curDepth + 1)
+        else:
+            legalValue = self.minValue(nextState, nextAgent, curDepth)
+        return legalValue
+    def maxValue(self, gameState, agentIndex, curDepth):
+        if( self.terminalState(gameState, agentIndex, curDepth) ):
+            return self.evaluationFunction(gameState), None
+        nextAgent = (agentIndex + 1) % gameState.getNumAgents()
+        scores = list()
+        legalMoves = gameState.getLegalActions(agentIndex)
+        for legalAction in legalMoves:
+            legalValue = self.nextValue(gameState, agentIndex, curDepth, nextAgent, legalAction)
+            scores.append(legalValue)
+        bestScore = max(scores)
+        bestIndices = [index for index in range(len(scores)) if scores[index] == bestScore]
+        chosenIndex = random.choice(bestIndices)
+        return bestScore, legalMoves[chosenIndex]
+    def minValue(self, gameState, agentIndex, curDepth):
+        if( self.terminalState(gameState, agentIndex, curDepth) ):
+            return self.evaluationFunction(gameState)
+        nextAgent = (agentIndex + 1) % gameState.getNumAgents()
+        scores = list()
+        for legalAction in gameState.getLegalActions(agentIndex):
+            legalValue = self.nextValue(gameState, agentIndex, curDepth, nextAgent, legalAction)
+            scores.append(legalValue)
+        meanScore = sum(scores) / len(scores)
+        return meanScore
         # End your code (Part 3)
 
 
@@ -175,7 +288,60 @@ def betterEvaluationFunction(currentGameState):
     evaluation function (Part 4).
     """
     # Begin your code (Part 4)
-    raise NotImplementedError("To be implemented")
+    #raise NotImplementedError("To be implemented")
+    pacemanDir = currentGameState.getPacmanState().getDirection()
+    pacmanPos = currentGameState.getPacmanPosition()
+    ghostsState = [(manhattanDistance(pacmanPos, currentGameState.getGhostPosition(Id)), Id) for Id in range(1, currentGameState.getNumAgents())]
+    minGhostDist, minGhostId = (0, 0) if len(ghostsState) == 0 else min(ghostsState)
+    ghostsDist = [x for x, y in ghostsState]
+    avgGhostDist = 0 if len(ghostsDist) == 0 else sum (ghostsDist) / len(ghostsDist)
+    isEat = currentGameState.data.agentStates[0].scaredTimer > 1
+    curScore = currentGameState.getScore()
+    foodDist = [manhattanDistance(pacmanPos, food) for food in currentGameState.getFood().asList()]
+    numFood = currentGameState.getNumFood()
+    minFoodDist = 1 if numFood == 0 else min(foodDist)
+    avgFoodDist = 1 if numFood == 0 else sum(foodDist) / len(foodDist)
+    capsulesDist = [manhattanDistance(pacmanPos, capsule) for capsule in currentGameState.getCapsules()]
+    minCapsuleDist = 1 if len(currentGameState.getCapsules()) == 0 else min( capsulesDist )
+    wall = currentGameState.hasWall(0,0)
+    numAgent = currentGameState.getNumAgents() > 2
+    """
+    if isEat:
+        return -60 * minGhostDist + curScore
+    elif minCapsuleDist < 2 and minGhostDist < 3:
+        return 300
+    elif minGhostDist < 1:
+        return curScore - 500
+    else:
+        return -1 * ( 1 / ( numFood + 1 ) ) * ( minFoodDist +  0.6 * avgFoodDist)  + curScore
+    """
+    """
+    if isEat:
+        return -50 * minGhostDist + 100 * curScore
+    elif minGhostDist < 2:
+        return 0.8 * curScore + avgGhostDist * numAgent
+    elif len(currentGameState.getCapsules()) > 0:
+        return 1 / minCapsuleDist
+    else:
+        return 0.5 * (1 / minFoodDist) + 1 / minCapsuleDist + curScore
+    """
+    #return isEat * -50 * minGhostDist + 100 * curScore + 0.8 * curScore + avgGhostDist * numAgent + 0.5 * (1 / minFoodDist) + 1 / minCapsuleDist + curScore
+    """
+    if isEat:
+        return 1 / minGhostDist
+    elif minGhostDist < 2:
+        return 1/ (minGhostDist+1)
+    elif len(capsulesDist) > 0:
+        return 1 / capsulesDist[len(capsulesDist) - 1]
+    else:
+        return 1 / minFoodDist
+    """
+    if isEat:
+        return -30 * minGhostDist + curScore
+    elif minGhostDist < 3:
+        return curScore + avgGhostDist * numAgent
+    else:
+        return -1 * minFoodDist + -20 * minCapsuleDist + curScore
     # End your code (Part 4)
 
 # Abbreviation
